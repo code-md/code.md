18e607ad2fb79bb21c9e0c52ba13bf92ffe96d93
dan <dan@wat.(none)>
2014-04-13 23:10:01 +0300

    enable south in project

--- codemd/settings.py
+++ codemd/settings.py
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'cruiser',
+    'south',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
